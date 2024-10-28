d13d341f603be80fb8a9ccb8fb30ef72af7a217a
awdfaf <awdfaf@kakao.com>
2024-10-28 22:10:13 +0900

    팔로잉 기능 구현

--- src/main/java/part1/community_feed/user/domain/User.java
+++ src/main/java/part1/community_feed/user/domain/User.java
@@ -7,13 +7,45 @@ public class User {
 //    private final String name;
 //    private final String profileImageUrl;
     private final UserInfo info;
+    private final UserRelationCounter followingCount;
+    private final UserRelationCounter followerCount;
 
     public User(Long id, UserInfo userInfo) {
         this.id = id;
 //        this.name = name;
 //        this.profileImageUrl = profileImageUrl;
         this.info = userInfo;
+        this.followingCount = new UserRelationCounter();
+        this.followerCount = new UserRelationCounter();
     }
+
+    public void follow(User targetUser) {
+        if (this.equals(targetUser)) {
+            throw new IllegalArgumentException("자기 자신을 팔로우할 수 없습니다.");
+        }
+        followingCount.increase();
+        // 디미터(Demeter)의 법칙 위반 -> private 메소드로 해결
+        //targetUser.followerCount.increase();
+        targetUser.increaseFollowerCount();
+    }
+
+    public void unfollow(User targetUser) {
+        if (this.equals(targetUser)) {
+            throw new IllegalArgumentException("자기 자신과의 팔로우를 취소할 수 없습니다.");
+        }
+        followingCount.decrease();
+        //targetUser.followerCount.decrease();
+        targetUser.decreaseFollowerCount();
+    }
+
+    private void increaseFollowerCount() {
+        followerCount.increase();
+    }
+
+    private void decreaseFollowerCount() {
+        followerCount.decrease();
+    }
+
 
     // id를 통해 객체를 구분해야함
     // equals와 hashCode를 오버라이드 해야함
